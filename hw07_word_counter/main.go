91c75e15c1d7a70872b1ef6002249ee463c829b3
Vitaliy <vitalikir156@gmail.com>
2024-05-28 18:32:26 +0300

    initial commit- hw07 counter

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 func main() {
-	// Place your code here.
+	fmt.Println(CountWords("Раз раз    ,;        -089u0 ad [0o9t\rshb86574]   раз два три два ТрИ \nТри рАрз раз"))
+}
+
+func CountWords(instring string) map[string]int {
+	var instringnew string
+	instring = strings.ToLower(instring)
+	instring = strings.ReplaceAll(instring, "\n", " ")
+	instring = strings.ReplaceAll(instring, "—", " ")
+	regexp := regexp.MustCompile(`[[:punct:]]|[[:cntrl:]]|[[:digit:]]`)
+	instring = regexp.ReplaceAllString(instring, "")
+	for instring != instringnew { // этот быдлокод призван убрать пробелы
+		instringnew = instring
+		instring = strings.ReplaceAll(instring, "  ", " ")
+	}
+	stringmap := map[string]int{}
+	splitstring := strings.Split(instringnew, " ")
+	for _, word := range splitstring {
+		val, ok := stringmap[word]
+		if ok {
+			stringmap[word] = val + 1
+		} else {
+			stringmap[word] = 1
+		}
+	}
+	return stringmap
 }
