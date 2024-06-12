d70368d2d6678bb15b4c69263fc55dadba129243
Vitaliy <vitalikir156@gmail.com>
2024-06-12 09:32:35 +0300

    json.* refactored

--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -16,31 +16,26 @@ type Book struct {
 	Size   float32 `json:"size"`
 	Rate   float32 `json:"rate"`
 }
-type Marshaller interface {
-	Marshal() []byte
-}
-type Unmarshaller interface {
-	Unmarshal() Book
+
+func (b Book) Marshal() ([]byte, error) {
+	e, err := json.Marshal(b)
+	return e, err
 }
 
-func (b Book) Marshal() []byte {
-	e, _ := json.Marshal(b)
-	return e
+func (b *Book) Unmarshal(data []byte) error {
+	err := json.Unmarshal(data, &b)
+	return err
 }
 
-func Unmarshal(b []byte) Book {
-	var data Book
-	json.Unmarshal(b, &data)
-	return data
+func MarshalSlice(b []Book) ([]byte, error) {
+	e, err := json.Marshal(b)
+	return e, err
 }
 
-func Slicemarshall(b []Book) []byte {
-	s := make([]byte, 0)
-	for _, book := range b {
-		u := Marshaller.Marshal(book)
-		s = append(s, u...)
-	}
-	return s
+func UnmarshalSlice(data []byte) ([]Book, error) {
+	var book []Book
+	err := json.Unmarshal(data, &book)
+	return book, err
 }
 
 func main() {
@@ -49,18 +44,21 @@ func main() {
 		{1, "title1", "author", 1991, 12, 4},
 		{2, "title2", "author2", 1992, 22, 3},
 	}
-
-	s := Marshaller.Marshal(b)
+	fmt.Println(slb)
+	s, _ := Book.Marshal(b)
 	// fmt.Println(s)
-	fmt.Printf(" %s\n", s)
-	slbum := Slicemarshall(slb)
-	fmt.Printf("slicem %s\n", slbum)
-	e := Unmarshal(s)
-	fmt.Println(e)
+	fmt.Printf("s %s\n", s)
+	var e Book
+	e.Unmarshal(s)
+	fmt.Printf("e.unmarsh %v\n", e)
+
+	slm, _ := MarshalSlice(slb)
+	fmt.Printf("slicemar %s\n", slm)
+	slu, _ := UnmarshalSlice(slm)
+	fmt.Printf("sliceunmar %v\n", slu)
 
 	pbook := &pb.Book{
-		ID: 3,
-
+		Id:     3,
 		Title:  "title1",
 		Author: "author",
 		Year:   1991,
@@ -69,7 +67,7 @@ func main() {
 	}
 	pbook.ProtoMessage()
 	data, _ := proto.Marshal(pbook)
-	fmt.Printf(" %s\n", data)
+	// fmt.Printf(" %s\n", data)
 	pbook2 := &pb.Book{}
 	proto.Unmarshal(data, pbook2)
 	fmt.Println(pbook2)
