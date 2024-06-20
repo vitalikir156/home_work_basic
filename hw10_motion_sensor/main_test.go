b460f5815a8a8c3fc106607fc150ab6571f605cf
Vitaliy <vitalikir156@gmail.com>
2024-06-20 16:26:59 +0300

    removed stopper, sensor and sensavearage refactored, added tests

--- hw10_motion_sensor/main_test.go
+++ hw10_motion_sensor/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -13,22 +15,42 @@ func TestAvearage(t *testing.T) {
 		expected int
 	}{
 		{"1", []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5},
-		{"2", []int{9, 9, 9, 9, 9, 9, 9, 9, 9, 9}, 9},
+		{"2", []int{9, 9, 9, 9, 9}, 9},
 		{"3", []int{4, 3, 2, 1, 2, 3, 4, 3, 1, 2}, 2},
 		{"4", []int{9, 8, 9, 7, 6, 5, 7, 9, 9, 6}, 7},
 	}
 	for _, tc := range tests {
-		sensdirtydata := make(chan int, 1)
-		sensaverdata := make(chan int, 1)
-		go sensavearage(sensdirtydata, sensaverdata)
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
+			sensdirtydata := make(chan int, 1)
+			sensaverdata := sensavearage(sensdirtydata)
 			for _, v := range tc.input {
 				sensdirtydata <- v
 			}
-
+			close(sensdirtydata)
 			require.Equal(t, tc.expected, <-sensaverdata)
 		})
-		close(sensdirtydata)
 	}
+}
+
+func TestSensor(t *testing.T) {
+	ta := time.After(time.Minute)
+
+	sensdirtydata := sensor()
+	var err error
+	go func() {
+		for task := range sensdirtydata {
+			if 10 < task || task < 0 {
+				err = fmt.Errorf("out of range: %v", task)
+			}
+		}
+	}()
+
+	<-ta // wait minute
+	_, ok := <-sensdirtydata
+	if ok {
+		err = fmt.Errorf("timedout but channel is open")
+	}
+
+	require.NoError(t, err)
 }
